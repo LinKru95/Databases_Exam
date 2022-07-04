f9847b6e234d7adb950045313a3c0d7b36cf2f2d
LinKru95 <100715343+LinKru95@users.noreply.github.com>
2022-07-04 08:31:32 +0300

    Initial create

---
+++ Databases_Exam/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Databases_Exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
